c651c4af5847d91748e9aaa4a93fd09b531dad16
xb6022801 <binxuqz@gmail.com>
2018-03-14 08:08:24 +0100

    commit

--- temp.js
+++ temp.js
@@ -24,3 +24,35 @@ document.body.addEventListener('click', function (event) {
   console.log(event.target == document.getElementById('btn')) // true
   console.log(event.eventPhase) // 3: 冒泡
 })
+
+//
+var container = document.querySelector('.container')
+container.addEventListener('scroll', function() {
+  console.log('???')
+})
+
+//test mouseleave, mouseout区别： 冒泡
+var container = document.querySelector('.container')
+var wraper = document.querySelector('.wraper')
+var fn = function(event) {
+  console.log(event.target)
+}
+
+container.addEventListener('mouseout', function() {
+  console.log('container mouseout')
+})
+container.addEventListener('mouseover', function() {
+  console.log('container mouseover')
+})
+wraper.addEventListener('mouseout', function() {
+  console.log('wraper out')
+})
+
+//
+document.documentElement.addEventListener('mousemove', function(e) {
+  console.log('client: ' + e.clientX +', ' + e.clientY)
+  console.log('page: ' + e.pageX +', ' + e.pageY)
+  console.log('screen: ' + e.screenX +', ' + e.screenY)
+  console.log(e.shiftKey || e.metaKey || e.ctrlKey || e.altKey)
+})
+
